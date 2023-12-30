a5dbe081398ca071976bf27d7244bf54230126be
Yaswanth14 <yaswanth14333@gmail.com>
2023-12-30 09:07:49 +0530

    solved and tested week-3 middleware problems

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,19 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use(function(req, res, next){
+  const userId = req.headers["user-id"];
+  
+  if (!numberOfRequestsForUser[userId]){
+    numberOfRequestsForUser[userId]=1;
+  }
+  else numberOfRequestsForUser[userId]++;
+
+  if(numberOfRequestsForUser[userId]>5) res.status(404).send("Request Forbiden");
+
+  next();
+})
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
